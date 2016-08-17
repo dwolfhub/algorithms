4b2eabae0665ba35b27deb36b7e09064485eb967
Daniel Wolf <danielrwolf5@gmail.com>
2016-08-17 18:38:32 -0500

    refactored binary search

--- search.py
+++ search.py
@@ -5,41 +5,34 @@ class BinarySearch(object):
 
     def __init__(self, list):
         self.list = list
-        self.guesses = 0
+        self.list_len = len(list)
 
-    def guess_halfway(self, x, min_index, max_index):
-        """
-        determines the index in between min and max, guesses that,
-        then modifies the min and max and calls itself
-        """
-        self.guesses = self.guesses + 1
-        index_to_try = int((min_index + max_index) / 2)
-
-        if index_to_try == min_index:
-            return x == self.list[min_index] or x == self.list[max_index]
-
-        if self.list[index_to_try] == x:
-            return true
-        elif x > self.list[index_to_try]:
-            return self.guess_halfway(x, index_to_try + 1, max_index)
-        elif x < self.list[index_to_try]:
-            return self.guess_halfway(x, min_index, index_to_try - 1)
-
-    def search(self, item):
-        self.guesses = 0
+    def in_list(self, item):
         """accepts an argument and determines if the item in in the ordered list"""
+        guesses = 0
         min_index = 0
-        max_index = len(self.list) - 1
+        max_index = self.list_len - 1
 
-        return self.guess_halfway(item, min_index, max_index), self.guesses
+        while min_index <= max_index:
+            guess = int((min_index + max_index) / 2)
+            guesses = guesses + 1
+
+            if self.list[guess] == item:
+                return True, guesses
+            elif self.list[guess] < item:
+                min_index = guess + 1
+            else:
+                max_index = guess - 1
+
+        return False, guesses
 
 
 ol = BinarySearch([2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97])
 
-print ol.search(1)
-print ol.search(12)
-print ol.search(61)
-print ol.search(11)
-print ol.search(100)
-print ol.search(10000)
+print ol.in_list(1)
+print ol.in_list(12)
+print ol.in_list(61)
+print ol.in_list(11)
+print ol.in_list(100)
+print ol.in_list(10000)
 
